7ff8a907a0c35ce0acde031df53308af16c91f41
Noah Andria <noahandriamanana41@.com>
2026-07-01 21:53:59 +0300

    logique d'insertion de vente

--- src/main/java/com/example/demo/controller/VenteController.java
+++ src/main/java/com/example/demo/controller/VenteController.java
@@ -1,16 +1,25 @@
 package com.example.demo.controller;
 
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.example.demo.entity.Vente;
+import com.example.demo.service.VenteService;
 
 
 
 @Controller
 public class VenteController {
+private final VenteService venteService;
 
+    public VenteController(VenteService venteService) {
+        this.venteService = venteService;
+    }
 
 @GetMapping("/vente/nouveau")
 public String nouveau(Model model) {
@@ -18,10 +27,24 @@ public String nouveau(Model model) {
     return "vente/form";
 }
 
-@PostMapping("/vente/nouveau")
-public String insertion(Model model) {
-    model.addAttribute("vente", new Vente());
-    return "vente/form";
+@PostMapping("/vente")
+public String save(@ModelAttribute Vente vente,
+                   RedirectAttributes redirectAttributes) {
+
+    try {
+
+        venteService.effectuerVente(vente.getQuantiteLait(),vente.getPrixUnitaire(),vente.getDateVente());
+
+        redirectAttributes.addFlashAttribute("success","Vente enregistrée.");
+
+    } catch (RuntimeException e) {
+
+        redirectAttributes.addFlashAttribute("error",e.getMessage());
+
+        return "redirect:/vente/nouveau";
+    }
+
+    return "redirect:/vente";
 }
 
 
---
+++ src/main/java/com/example/demo/service/VenteService.java
@@ -0,0 +1,92 @@
+package com.example.demo.service;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.example.demo.entity.Production;
+import com.example.demo.entity.Vente;
+import com.example.demo.repository.ProductionRepository;
+import com.example.demo.repository.UtilisateurRepository;
+import com.example.demo.repository.VenteRepository;
+
+@Service
+@Transactional
+public class VenteService {
+
+    private final ProductionRepository productionRepository;
+    private final VenteRepository venteRepository;
+    private final UtilisateurRepository utilisateurRepository;
+
+    public VenteService(
+            ProductionRepository productionRepository,
+            VenteRepository venteRepository,
+            UtilisateurRepository utilisateurRepository) {
+
+        this.productionRepository = productionRepository;
+        this.venteRepository = venteRepository;
+        this.utilisateurRepository = utilisateurRepository;
+    }
+
+    public void effectuerVente(BigDecimal quantite,BigDecimal prixUnitaire, LocalDate dateVente) {
+
+
+
+        BigDecimal stock = productionRepository.getRemainingStock();
+
+        if (stock.compareTo(quantite) < 0) {
+            throw new RuntimeException(
+                    "Stock insuffisant. Stock actuel : " + stock + " L");
+        }
+
+        List<Production> productions = productionRepository.findByQuantiteRestanteGreaterThanOrderByDateProductionAsc(BigDecimal.ZERO);
+
+        BigDecimal restant = quantite;
+
+        List<Production> modified = new ArrayList<>();
+
+        for (Production production : productions) {
+
+            if (restant.compareTo(BigDecimal.ZERO) <= 0) {
+                break;
+            }
+
+            BigDecimal disponible = production.getQuantiteRestante();
+
+            if (disponible.compareTo(restant) >= 0) {
+
+                production.setQuantiteRestante(disponible.subtract(restant));
+
+                restant = BigDecimal.ZERO;
+
+            } else {
+
+                restant = restant.subtract(disponible);
+
+                production.setQuantiteRestante(BigDecimal.ZERO);
+            }
+
+            modified.add(production);
+        }
+
+        productionRepository.saveAll(modified);
+
+        Vente vente = new Vente();
+
+        vente.setDateVente(dateVente);
+        vente.setQuantiteLait(quantite);
+        vente.setPrixUnitaire(prixUnitaire);
+        vente.setCreatedAt(LocalDateTime.now());
+
+        // Temporary
+        vente.setCreatedBy( utilisateurRepository.findById(1L).orElseThrow());
+
+        venteRepository.save(vente);
+    }
+
+}
