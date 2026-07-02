f41f6b45ea545f2d00135f753a8ef527854c7453
Elisa <elisaharimalala07@gmail.com>
2026-07-02 19:54:27 +0300

    ajout mini dash

--- src/main/java/com/example/demo/repository/RefStatutVacheRepository.java
+++ src/main/java/com/example/demo/repository/RefStatutVacheRepository.java
@@ -3,6 +3,8 @@ package com.example.demo.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.demo.entity.RefStatutVache;
+import java.util.Optional;
 
 public interface RefStatutVacheRepository extends JpaRepository<RefStatutVache, Integer> {
+	Optional<RefStatutVache> findByCode(String code);
 }
